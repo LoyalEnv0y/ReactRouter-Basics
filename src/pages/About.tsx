f377a91404de42b95f134585014a15b4d2b00692
LoyalEnv0y <cetin200012@gmail.com>
2023-08-25 22:25:46 +0300

    Replaced NavLinks, Renamed images and parent file, fixed image imports
    Replaced handmade NavLinks with native React-Router NavLinks for better use.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,15 @@
+import Footer from '../components/Footer';
 import Navbar from '../components/Header';
 
 const About = () => {
 	return (
-		<>
-			<Navbar />
-			<h1>About Page!</h1>
-		</>
+		<div className='h-screen flex flex-col'>
+			<Navbar className='flex-none' />
+			<main className='flex-1'>
+				<img src="images/Night-Sky-Caravan.png" alt="banner" />
+			</main>
+			<Footer className='flex-none' />
+		</div>
 	);
 };
 

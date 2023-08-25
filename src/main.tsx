7e112209c2d0b446e83f1fd0d64a696c9ca0a466
LoyalEnv0y <cetin200012@gmail.com>
2023-08-25 14:50:52 +0300

    Created basic routes and pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,32 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About.tsx';
+import Contact from './pages/Contact.tsx';
+import Prices from './pages/Prices.tsx';
+import Home from './pages/Home.tsx';
+
+/*
+	-------------------------------------- 🌐 --------------------------------------
+	BrowserRoute is a top level provider that parses all the routes.
+
+	-------------------------------------- 🪧 --------------------------------------
+	Routes is an array containing multiple routes.
+
+	-------------------------------------- 📍 --------------------------------------
+	Route is the ultimate way of showing different pages. the path prop defines
+	where the route will go to and the element prop defines what to show.
+*/
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
-)
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/prices" element={<Prices />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);

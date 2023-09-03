b2f69c88fbba8667f9b2d79e70fb9d656ced5807
LoyalEnv0y <cetin200012@gmail.com>
2023-09-03 17:49:18 +0300

    Refactored the code of Vans page so that it uses native useSearchParams of React Native

--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -1,29 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Button from '../components/Button';
 import classnames from 'classnames';
-import { v4 as uuid } from 'uuid';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { Van } from '../types';
 import { capitalize } from '../utils';
 
-const initialFilters = [
-	{
-		id: uuid(),
-		name: 'simple',
-		active: false,
-	},
-	{ id: uuid(), name: 'luxury', active: false },
-	{
-		id: uuid(),
-		name: 'rugged',
-		active: false,
-	},
-];
-
 const Vans = () => {
 	const [vans, setVans] = useState<Van[]>([]);
-	const [filteredVans, setFilteredVans] = useState<Van[]>([]);
-	const [filters, setFilters] = useState(initialFilters);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const types = useMemo(() => {
+		return searchParams.get('types')?.split(',') || [];
+	}, [searchParams]);
 
 	// TODO: Do all the data fetching in a API file.
 	useEffect(() => {
@@ -33,7 +20,11 @@ const Vans = () => {
 				if (!resp.ok) throw new Error(resp.statusText);
 
 				const respJSON = await resp.json();
-				setVans(respJSON.vans);
+				const filteredVans = types?.length
+					? respJSON.vans.filter((van: Van) => types.includes(van.type))
+					: respJSON.vans;
+
+				setVans(filteredVans);
 			} catch (err) {
 				console.log('Error => ', err);
 				return <div>There was an error</div>;
@@ -41,59 +32,58 @@ const Vans = () => {
 		};
 
 		fetchData();
-	}, []);
+	}, [types]);
 
-	useEffect(() => {
-		const activeFilters = filters
-			.filter((filter) => filter.active)
-			.map((filter) => filter.name);
-			
-		if (activeFilters.length < 1) {
-			setFilteredVans(vans);
-			return;
+	const updateActiveTypes = (typeName: string) => {
+		const idx = types.indexOf(typeName);
+		if (idx > -1) {
+			types.splice(idx, 1);
+			if (types.length < 1) {
+				clearTypes();
+				return;
+			}
+		} else {
+			types.push(typeName);
 		}
 
-		setFilteredVans(vans.filter((van) => activeFilters.includes(van.type)));
-	}, [filters, vans]);
-
-	const updateActiveFilters = (id: string) => {
-		setFilters(
-			filters.map((filter) =>
-				filter.id === id ? { ...filter, active: !filter.active } : filter
-			)
-		);
+		searchParams.set('types', types.join(','));
+		setSearchParams(searchParams);
 	};
 
-	const clearFilters = () => {
-		setFilters(filters.map((filter) => ({ ...filter, active: false })));
+	const clearTypes = () => {
+		setSearchParams({});
+	};
+
+	const getClassesForButton = (typeName: string): string => {
+		return classnames('mr-3 last:mr-2 mb-1', `hover:${typeName}`, {
+			[`text-white bg-${typeName}`]: types?.includes(typeName),
+		});
 	};
 
 	const constructFilterButtons = () => {
 		const buttons = (
 			<div className="flex flex-wrap justify-between">
 				<div className="mb-1">
-					{filters.map((filter) => {
-						const finalClasses = classnames(
-							'mr-3 last:mr-2 mb-1',
-							`hover:${filter.name}`,
-							{
-								[`text-white bg-${filter.name}`]: filter.active,
-							}
-						);
-						return (
-							<Button
-								className={finalClasses}
-								key={filter.id}
-								onClick={() => updateActiveFilters(filter.id)}>
-								{capitalize(filter.name)}
-							</Button>
-						);
-					})}
+					<Button
+						className={getClassesForButton('simple')}
+						onClick={() => updateActiveTypes('simple')}>
+						Simple
+					</Button>
+					<Button
+						className={getClassesForButton('luxury')}
+						onClick={() => updateActiveTypes('luxury')}>
+						Luxury
+					</Button>
+					<Button
+						className={getClassesForButton('rugged')}
+						onClick={() => updateActiveTypes('rugged')}>
+						Rugged
+					</Button>
 				</div>
 
 				<button
 					className="text-sm underline underline-offset-[3px]"
-					onClick={clearFilters}>
+					onClick={clearTypes}>
 					Clear Filters
 				</button>
 			</div>
@@ -110,11 +100,12 @@ const Vans = () => {
 			</section>
 
 			<section className="my-8 flex flex-wrap justify-between">
-				{filteredVans.map((van) => {
+				{vans.map((van) => {
 					return (
 						<Link
 							to={`/vans/${van.id}`}
-							className="mb-6 w-[47%] min-w-[110px]">
+							className="mb-6 w-[47%] min-w-[110px]"
+							key={van.id}>
 							<img
 								src={van.imageUrl}
 								alt="van photo"

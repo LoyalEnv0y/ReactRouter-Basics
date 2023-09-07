0f6b86d4ae51ebc3dc479a505834cbb07b222d12
LoyalEnv0y <cetin200012@gmail.com>
2023-09-07 12:18:27 +0300

    Netlify fix v5

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer: FC<FooterProps> = ({ className }) => {
 		classnames(className)
 	);
 
-	const siteVersion = '0.4.21';
+	const siteVersion = '0.4.22';
 	return (
 		<footer className={classes}>
 			<section className="flex items-center gap-1">
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -24,10 +24,15 @@ export async function requireAuth() {
 			add the body. You do not need to do this in normal servers.
 
 			This comes with the type issue of reassigning the body. We can ignore this error for now.
+
+			Edit (07.09.2023;12:15): It seems that reassigning the body causes an issue with netlify
+			and it is preventing it from deploying. I will comment out the reassigning code. This
+			breaks the production as it throws an error with no message but at least it can be 
+			published now.
 		*/
 
 		const response = redirect('/login?message=You must be logged in');
-		response.body = true; 
+		// response.body = true; 
 		throw response;
 	}
 
